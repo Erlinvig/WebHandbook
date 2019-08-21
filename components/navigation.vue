3b80dbbbb115c593c642d0d4c48b671059247b94
erlinvig <erlinvig@gmail.com>
2019-08-21 16:56:45 +0300

    app: init

---
+++ components/navigation.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .wrapper
+    .container
+      .navigation
+        ul
+          li
+            nuxt-link.navigation__link(to="/") Главная
+        ul
+          li
+            nuxt-link.navigation__link(to="/") Вход
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+  .wrapper {
+    background-color: #170c2b;
+    padding: 1.7em;
+
+    .navigation {
+      display: flex;
+      justify-content: space-between;
+      &__link {
+        color: white;
+        font-size: 22px;
+        font-weight: 700;
+      }
+    }
+  }
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  .page
+
+</template>
+
+<script>
+import navigation from '~/components/navigation.vue'
+
+export default {
+  components: {
+    navigation
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
