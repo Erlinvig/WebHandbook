1be91a4ed72acd4dd7f37bff6ab038a0f471ecf6
erlinvig <erlinvig@gmail.com>
2019-08-26 13:20:59 +0300

    feat(server): include mongodb

--- server/app.js
+++ server/app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const keys = require('./keys');
 const app = express();
+
+mongoose.connect(keys.MONGO_URI, {useNewUrlParser: true})
+  .then(() => console.log('MongoDB connected...'))
+  .catch(error => console.error(error));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
---
+++ server/keys/keys.dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGO_URI: `mongodb+srv://erlinvig:octa7s7@cluster0-mz28e.gcp.mongodb.net/test?retryWrites=true&w=majority`,
+  JWT: 'dev-jwt-key'
+};
