9702db45fbac82c803534212470262fbe336a802
erlinvig <erlinvig@gmail.com>
2019-09-06 16:22:33 +0300

    fix(ui): change admin/content table to flexbox

--- components/admin/content/ContentTable.vue
+++ components/admin/content/ContentTable.vue
@@ -1,21 +1,25 @@
 <template lang="pug">
-  .content-table(v-if="getCurrentTechnology")
-    table
-      thead
-        tr
-          td
-            i(class="el-icon-arrow-left")
-          td(
-            v-for="chapter in getCurrentTechnology.chapters"
-            :key="chapter._id"
-            ) {{chapter.title}}
-          td
-            i(class="el-icon-arrow-right")
-      tbody
-        tr(v-for="(row, index) in getRows" :key="index")
-          td
-          td(v-for="(page, indexPage) in row" :key="indexPage" v-if="page") {{page.title}}
-          td
+  .table(v-if="getCurrentTechnology")
+    i.table__navigation(class="el-icon-arrow-left")
+    .wrapper
+      .table__content
+        .column(
+          v-for="(chapter, index) in getCurrentTechnology.chapters"
+          :key="index"
+        )
+          .column__title
+            span {{chapter.title}}
+            i(class="el-icon-edit")
+            i(class="el-icon-delete")
+          .column__page(
+            v-for="page in chapter.pages"
+          )
+            span {{page.title}}
+            i(class="el-icon-edit")
+            i(class="el-icon-delete")
+          .column__page-creator
+            span Создать страницу
+    i.table__navigation(class="el-icon-arrow-right")
 
 </template>
 
@@ -49,7 +53,7 @@
                 value: currentItem.pages.length
               } : result;
           })
-           : { value: null };
+          : { value: null };
 
         return result.value;
       }
@@ -79,5 +83,59 @@
 </script>
 
 <style lang="scss" scoped>
+  .table {
+    display: flex;
+    justify-content: space-between;
+    background-color: #fff;
+    border-radius: 5px;
+    padding: 10px;
+    &__navigation {
+      font-size: 16px;
+      min-width: 25px;
+      text-align: center;
+    }
+    .wrapper {
+      min-width: calc(100% - 50px);
+      overflow: hidden;
+    }
+    &__content {
+      display: flex;
+      justify-content: flex-start;
 
+
+      .column {
+        min-width: 33.33%;
+        &__title, &__page {
+          font-size: 16px;
+          display: flex;
+          justify-content: center;
+          span, i {
+            font-size: 16px;
+          }
+          span {
+            padding-right: 1.5em;
+          }
+          i {
+            padding-right: .5em;
+          }
+        }
+        &__title {
+          font-weight: 700;
+          padding-bottom: 5px;
+        }
+        &__page {
+          padding: 5px;
+        }
+        &__page-creator {
+          font-size: 16px;
+          display: flex;
+          justify-content: center;
+          padding: 5px;
+          span {
+            font-size: 16px;
+          }
+        }
+      }
+    }
+  }
 </style>
