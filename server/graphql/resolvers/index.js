9c473b3446557c428b70e3f163a2d5b9e2000b4b
erlinvig <erlinvig@gmail.com>
2019-09-10 21:37:19 +0300

    feat(ui): add user resolver

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -1,9 +1,11 @@
 const technology = require('./technology');
 const chapter = require('./chapter');
 const page = require('./page');
+const user = require('./user');
 
 module.exports = {
   ...technology,
   ...chapter,
-  ...page
+  ...page,
+  ...user
 };
---
+++ server/graphql/resolvers/user.js
@@ -0,0 +1,8 @@
+const Page = require('../../models/pages');
+const User = require('../../models/users');
+
+module.exports = {
+  users() {},
+  signin() {},
+  signup() {}
+};
